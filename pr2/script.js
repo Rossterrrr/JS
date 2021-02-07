cb013ae69c9c496f4b787dd8c4ef76cd6ba1cc81
vadim <vadim22052@gmail.com>
2021-02-07 16:49:13 +0300

    new practice2 javascript file

--- pr2/script.js
+++ pr2/script.js
@@ -1,1 +1,24 @@
-console.log(1);
+let numberOfFilms;
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+  count:numberOfFilms,
+  movie:{},
+  actors:{},
+  genres:[],
+  privat:false
+};
+
+for (let i = 0;i<2;i++){
+  let ans1 = prompt('Один из последних просмотренных фильмов?');
+  let ans2 = +prompt('Ваша оценка фильма?');
+  personalMovieDB.movie[ans1] = ans2;
+  if((ans1 == null) || (ans1.length > 50) || (ans2 == null)) i--;
+}
+if(personalMovieDB.count<10){
+  alert('Вы просмотрели мало фильмов');
+}else if(personalMovieDB.count<30){
+  alert('Вы классический зритель');
+}else alert('Вы киноман');
+
+console.log(personalMovieDB);
