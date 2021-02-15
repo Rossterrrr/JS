8556c39087735c9360fcfb5a8b803f484131c22e
vadim <vadim22052@gmail.com>
2021-02-15 08:58:15 +0300

    improved pr4

--- pr4/script.js
+++ pr4/script.js
@@ -1,1 +1,35 @@
-console.log(1);
+const obj ={
+    name:'test',
+    width:1024,
+    height:1024,
+    colors:{
+        border:'black',
+        background:'red'
+    },
+    makeTest:function(){
+        console.log('Test method');
+    }
+}
+
+obj.makeTest();
+
+console.log(obj.name);
+delete obj.name;
+let counter = 0;
+console.log(obj);
+for(let key in obj){
+    if(typeof(obj[key]) === 'object'){
+        for(let i in obj[key]){
+            console.log(`Свойство ${i} имеет значение ${obj[key][i]}`);
+            counter++;
+        }
+    }else {
+        console.log(`Свойство ${key} имеет значение ${obj[key]}`);
+        counter++;
+    }
+   
+}
+console.log(counter);
+
+const {border,background} = obj.colors;
+console.log(border,background);
